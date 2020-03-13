a33a4a7df280b907e21073d7c6e69ca014549d10
ElenaKiseljova <50455974+ElenaKiseljova@users.noreply.github.com>
2020-03-13 17:49:46 +0200

    +1

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ $(document).ready(function () {
     $(".static__iframe").eq(index).addClass("active");
     
     $(".popup").fadeOut();
+    
+    console.log(111);
   });
   
   $(".popup__close").click(function () {
     $(".popup").fadeIn();
     
     $(".static__iframe").removeClass("active");
+    
+    console.log(222);
   });
 });
