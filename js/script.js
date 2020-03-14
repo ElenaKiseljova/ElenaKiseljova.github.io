bfe3f4264dbd288a457b04843f773fd8f2c7cd46
Lena <e.a.kiseljova@gmail.com>
2020-03-14 15:51:35 +0200

    + Car & Yetycave

--- js/script.js
+++ js/script.js
@@ -1,19 +1,51 @@
 $(document).ready(function () {
+  // открытие модального окна с iframe
+  
   $(".static__button").click(function () {
     var index = $(this).parent().index();
     
     $(".static__iframe").eq(index).addClass("active");
     
-    $(".popup").fadeIn();
-    
-    console.log(111);
+    $(".popup").fadeIn();  
+    $(".popup-overlay").fadeIn(); 
+    $("html").css("overflow-y", "hidden");
   });
   
-  $(".popup__close").click(function () {
+  // закрытие модального окна с iframe (кнопка, фон)
+  
+  $(".popup__close, .popup-overlay").click(function () {
     $(".popup").fadeOut();
+    $(".popup-overlay").fadeOut();
+    $("html").css("overflow-y", "auto");
     
-    $(".static__iframe").removeClass("active");
+    $(".static__iframe").removeClass("active");    
+  });
+  
+  // закрытие модального окна с iframe (esc)
     
-    console.log(222);
+  $(window).keypress(function (e) { 
+      if ($(".static__iframe").hasClass("active")) {
+        if (e.keyCode == 27) {
+          e.preventDefault();
+          
+          $(".popup").fadeOut();
+          $(".popup-overlay").fadeOut();
+          $("html").css("overflow-y", "auto");
+          
+          $(".static__iframe").removeClass("active");      
+        }
+      }      
+    });
+    
+  // анимированный переход по якорям в навигационном меню
+  
+  $(".navigation__list--menu .navigation__link").click(function () {
+    
+    var element = $(this).attr("href");
+    var top =$(element).offset().top;
+    
+    console.log(element, top);
+    
+    $("html, body").animate({scrollTop: top}, 500);   
   });
 });
