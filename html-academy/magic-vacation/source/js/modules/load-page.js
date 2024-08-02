4595af032558520e2039f7c25576262ae2d78f60
Elena Kiseljova <elena.ki@webest.ru>
2024-08-02 15:09:34 +0300

    del: HTMLAcademy section

--- html-academy/magic-vacation/source/js/modules/load-page.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  window.addEventListener(`load`, () => {
-    const body = document.body || document.querySelector(`body`);
-
-    if (body) {
-      body.classList.add(`load`);
-    }
-  });
-};
